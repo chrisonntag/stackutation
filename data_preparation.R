895b417ae28a40b6f48e9027d5200725d53a2bc5
Lenard Strahringer <61589179+lenardst@users.noreply.github.com>
2023-06-22 10:30:50 +0200

    Add files via upload

---
+++ data_preparation.R
@@ -0,0 +1,7 @@
+# Import csv files first
+
+# Create dataset without the bodies of the questions
+questions_nobody = questions[,c(1:2,4:9)]
+
+save(questions, file = "questions.RData")
+remove(questions)
